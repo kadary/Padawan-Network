a2322fe0085f109a87833d17a89ba17e9a528cdb
kadary DEMBELE <kadary@demkada.com>
2014-11-24 16:16:20 +0100

    First Commit

---
+++ padawan/app/controllers/navigation_controller.rb
@@ -0,0 +1,13 @@
+class NavigationController < ApplicationController
+  def accueil
+  end
+
+  def profile
+  end
+
+  def message
+  end
+
+  def apropos
+  end
+end
---
+++ padawan/app/controllers/users_controller.rb
@@ -0,0 +1,20 @@
+class UsersController < ApplicationController
+  def new
+	@user = User.new
+  end
+
+  def create
+	@user = User.new(user_params)
+	if @user.save
+		session[:user_id]=@user.id
+		redirect_to root_url
+	else
+		render :action => "new"
+	end
+  end
+  
+  private
+   def user_params
+      params.require(:user).permit(:email, :first_name, :last_name, :password, :password_confirmation)
+    end
+end
