a9d27d406481a502fa3eac3ebf9a85925aa5730f
kadary DEMBELE <kadary@demkada.com>
2014-12-01 21:28:00 +0100

    Login API 1

--- padawan/app/controllers/sessions_controller.rb
+++ padawan/app/controllers/sessions_controller.rb
@@ -1,15 +1,36 @@
 class SessionsController < ApplicationController
+
+
+  def show  
+  end
+  
+  
   def new
 	@user = User.authenticate(params[:email], params[:password])
-	if @user
-		session[:user_id] = @user.id
-		redirect_to root_url
-	else
+	
+	respond_to do |format|
+      if @user
+		session[:user_id]=@user.id
+        format.html {redirect_to root_url }
+        format.json { render :show, status: :created, location: @user }
+      else
 		session[:user_id] = nil
-		render :action => "new"
-		flash[:notice] = "Les identifiants saisis ne sont pas corrects"
+        format.html { render :new, notice: "Les identifiants saisis ne sont pas corrects" }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
 	end
-  end
+	
+	
+#	if @user
+#		session[:user_id] = @user.id
+#		redirect_to root_url
+#	else
+#		session[:user_id] = nil
+#		render :action => "new"
+#		flash[:notice] = "Les identifiants saisis ne sont pas corrects"
+#	end
+#  end
 
   def destroy
 	session[:user_id] = nil
