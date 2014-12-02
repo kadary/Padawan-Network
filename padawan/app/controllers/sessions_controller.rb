fd724f3de0da2617b8528367075ed36029ed6571
kadary DEMBELE <kadary@demkada.com>
2014-12-02 10:53:28 +0100

    Sessions_controller mod test 8

--- padawan/app/controllers/sessions_controller.rb
+++ padawan/app/controllers/sessions_controller.rb
@@ -7,19 +7,20 @@ class SessionsController < ApplicationController
   
   def new
 	@user = User.authenticate(params[:email], params[:password])
-	
-	respond_to do |format|
-      if @user
-		session[:user_id]=@user.id
-        format.html {redirect_to root_url }
-        format.json { render :json => {:success => 1, :message => "Session ouverte", :status => "200"} }
-      else
+	if @user
+		session[:user_id]=@user.id	
+		respond_to do |format|
+			format.html {redirect_to root_url }
+			format.json { render :json => {:success => 1, :message => "Session ouverte", :status => "200"} }
+		end
+    else
 		session[:user_id] = nil
-        format.html { render :new, notice: "Les identifiants saisis ne sont pas corrects" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+		respond_to do |format|
+			format.html { render :new, notice: "Les identifiants saisis ne sont pas corrects" }
+			format.json { render json: @user.errors, status: :unprocessable_entity }
+		end
     end
-	end
+  end
 	
 	
 #	if @user
