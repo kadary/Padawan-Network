76ed6309da94dba78e5115048f9adc37f873aa76
kadary DEMBELE <kadary@demkada.com>
2014-12-02 15:24:16 +0100

    User_controller mod 4

--- padawan/app/controllers/users_controller.rb
+++ padawan/app/controllers/users_controller.rb
@@ -4,8 +4,8 @@ class UsersController < ApplicationController
   end
 
   def create
-	@user = User.new(user_params)
-	Rails::logger.debug user_params
+	@user = User.create(user_params)
+	#Rails::logger.debug user_params
 	if @user.save
 		session[:user_id]=@user.id
 		@userSession = @user.id 
