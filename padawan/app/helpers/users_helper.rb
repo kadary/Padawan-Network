3daea5b39e90d13f6884646f7eb3a80201cc64ac
kadary DEMBELE <kadary@demkada.com>
2014-12-16 13:34:36 +0100

    Some changes

--- padawan/app/helpers/users_helper.rb
+++ padawan/app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+	# Returns the Gravatar for the given user.
+	def gravatar_for(status)
+		@user = User.find(status.posted_by_uid)
+		gravatar_id = Digest::MD5::hexdigest(@user.email.downcase)
+		gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+		image_tag(gravatar_url, alt: @user.first_name, class: "statusimg")
+  end
 end
